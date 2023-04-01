81197557b4f710f29af24f12642cb5b8f1a83090
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-01 09:07:02 +0700

    1.0.0

---
+++ DoubleExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Doubles
+{
+    public static class DoubleExtension
+    {
+        public static double SqrMagnitude(this Double3 value)
+        {
+            return value.X * value.X + value.Y * value.Y + value.Z * value.Z;
+        }
+    }
+}
